9bae195ccc23f9650b13aa0ed1b9a59fa2881ea6
Roberto Andrade F <randrade@ignea-dell.huawei.net>
2021-03-30 10:45:34 -0600

    Nueva base de datos.

--- client.js
+++ client.js
@@ -10,10 +10,25 @@ const pgclient = new Client({
 
 pgclient.connect();
 
+// Creamos la base de datos de prueba
+const create_db = 'CREATE DATABASE prueba'
 
+pgclient.end();
+
+// Creamos la tabla
 const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
 
 
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'prueba'
+});
+
+pgclient.connect();
+// Primer insert
 const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
 const values = ['Ingrid', 'Chávez', 18, 'Reforms 222. Cuauhtémoc, CDMX, México', 'ingrid@github.com']
 
@@ -26,6 +41,7 @@ pgclient.query(text, values, (err, res) => {
 });
 
 
+// Segundo insert
 const text1 = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
 const values1 = ['Roberto', 'Andrade', 58, 'Chopo 8, Cuauhtémoc, CDMX, México', 'raf@github.com']
 
